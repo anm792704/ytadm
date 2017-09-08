ba20b9335ed93e61727f91eb84e7742a479b90d3
anm792704 <anm792704@googlemail.com>
2017-09-08 21:12:15 +0200

    searches added

--- ytadm.py
+++ ytadm.py
@@ -63,7 +63,7 @@ def channels_list_by_username(service, **kwargs):
 def remove_empty_kwargs(**kwargs):
   good_kwargs = {}
   if kwargs is not None:
-    for key, value in kwargs.iteritems():
+    for key, value in kwargs.items():
       if value:
         good_kwargs[key] = value
   return good_kwargs
@@ -74,12 +74,23 @@ def print_results(results):
 
 
 def channels_list_by_id(service, **kwargs):
-  #kwargs = remove_empty_kwargs(**kwargs) # See full sample for function
+  kwargs = remove_empty_kwargs(**kwargs) # See full sample for function
   results = service.channels().list(
     **kwargs
   ).execute()
 
   print_results(results)
+
+def videos_list_by_id(service, **kwargs):
+    kwargs = remove_empty_kwargs(**kwargs)
+    results = service.videos().list(**kwargs).execute()
+    print_results(results)
+
+
+def search_list_by_keyword(service, **kwargs):
+    kwargs = remove_empty_kwargs(**kwargs)
+    results = service.search().list(**kwargs).execute()
+    print_results(results)
 
 #channels_list_by_id(service,
 #    part='snippet,contentDetails,statistics',
@@ -87,10 +98,96 @@ def channels_list_by_id(service, **kwargs):
 
 #channels_list_by_username(service, part='snippet,contentDetails,statistics', forUsername='ThomasDollar1')
 
-channels_list_by_id(service,
-    part='snippet,contentDetails,statistics',
-    id='Z0DJ1UBmS1sEjDBr2OABAw')
+#channels_list_by_id(service,
+#    part='snippet,contentDetails,statistics',
+#    id='Z0DJ1UBmS1sEjDBr2OABAw')
+
+
+def build_resource(properties):
+  resource = {}
+  for p in properties:
+    # Given a key like "snippet.title", split into "snippet" and "title", where
+    # "snippet" will be an object and "title" will be a property in that object.
+    prop_array = p.split('.')
+    ref = resource
+    for pa in range(0, len(prop_array)):
+      is_array = False
+      key = prop_array[pa]
+      # Convert a name like "snippet.tags[]" to snippet.tags, but handle
+      # the value as an array.
+      if key[-2:] == '[]':
+        key = key[0:len(key)-2:]
+        is_array = True
+      if pa == (len(prop_array) - 1):
+        # Leave properties without values out of inserted resource.
+        if properties[p]:
+          if is_array:
+            ref[key] = properties[p].split(',')
+          else:
+            ref[key] = properties[p]
+      elif key not in ref:
+        # For example, the property is "snippet.title", but the resource does
+        # not yet have a "snippet" object. Create the snippet object here.
+        # Setting "ref = ref[key]" means that in the next time through the
+        # "for pa in range ..." loop, we will be setting a property in the
+        # resource's "snippet" object.
+        ref[key] = {}
+        ref = ref[key]
+      else:
+        # For example, the property is "snippet.description", and the resource
+        # already has a "snippet" object.
+        ref = ref[key]
+  return resource
+
+# Remove keyword arguments that are not set
+def remove_empty_kwargs(**kwargs):
+  good_kwargs = {}
+  if kwargs is not None:
+    for key, value in kwargs.items():
+      if value:
+        good_kwargs[key] = value
+  return good_kwargs
+
+### END BOILERPLATE CODE
+
+# Sample python code for playlists.list
+
+def playlists_list_by_channel_id(service, **kwargs):
+  kwargs = remove_empty_kwargs(**kwargs) # See full sample for function
+  results = service.playlists().list(
+    **kwargs
+  ).execute()
+
+  print_results(results)
+
+playlists_list_by_channel_id(service,
+    part='snippet,contentDetails',
+    channelId='UCZ0DJ1UBmS1sEjDBr2OABAw',
+    maxResults=25)
+
 
 channels_list_by_id(service,
     part='snippet,contentDetails,statistics',
     id='UCZ0DJ1UBmS1sEjDBr2OABAw')
+
+videos_list_by_id(service, part='snippet, contentDetails, statistics',
+                  id='75-aN75C3Y0')
+
+search_list_by_keyword(service,
+                        part='snippet',
+                        maxResults=25,
+                        q='TBH.eSports',
+                        type=' ')
+
+def playlist_items_list_by_playlist_id(service, **kwargs):
+  kwargs = remove_empty_kwargs(**kwargs) # See full sample for function
+  results = service.playlistItems().list(
+    **kwargs
+  ).execute()
+
+  print_results(results)
+
+playlist_items_list_by_playlist_id(service,
+    part='snippet,contentDetails',
+    maxResults=25,
+    playlistId='PL0qwFDY6TCnxnYiOVi5SK5XMinZRR9wIE')
